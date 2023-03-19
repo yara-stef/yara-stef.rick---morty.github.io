204707ab84a0a0ef23f9d7a29c329e3628c42e63
yara-stef <ymhladyak@gmail.com>
2023-03-19 22:09:39 +0000

    workimg on filter functionality

--- rick-morty/src/features/FilterByName.jsx
+++ rick-morty/src/features/FilterByName.jsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import { useState } from 'react';
 
 function FilterByName() {
-    
+    const [query, setQuery] = useState('');
+    console.log(query);
     return (
         <form className='search'>
              <input className='filter'
                 type='search'
                 placeholder='Filter by name...'
+                onChange={(e) => setQuery(e.target.value)}
             >                
             </input>
         </form>
